9e4eeabe0296feaa4884dca6df1ae08260dcbead
maheshLEO4 <maheshmahi5345@gmail.com>
2026-06-10 12:17:21 +0530

    Added frontend origin

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
 
 from auth_routes import auth_app
 from notes_routes import notes_app
 
+load_dotenv()
+
 app = FastAPI()
+
+frontend_origin = os.getenv("FRONTEND_URL")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React dev server
+    allow_origins=[frontend_origin] if frontend_origin else [],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -16,4 +22,3 @@ app.add_middleware(
 
 app.mount("/auth", auth_app)
 app.mount("/notes", notes_app)
-
